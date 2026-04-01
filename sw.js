3eddadee0e9705427c30e6c4be0f186d45fa589c
youdaguwu169-web <youdaguwu169@gmail.com>
2026-04-01 23:11:13 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
-self.addEventListener('install', () => {
-  self.skipWaiting();
+self.addEventListener('push', function(event) {
+    const data = event.data.json();
+    self.registration.showNotification(data.title, {
+        body: data.body,
+        icon: 'icon.png'
+    });
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
 });
